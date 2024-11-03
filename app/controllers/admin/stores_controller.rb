d2922894c9e2ca4488f06027eb2a2d7c6924196f
EC2 Default User <ec2-user@ip-172-31-16-243.ap-northeast-1.compute.internal>
2024-11-03 10:35:20 +0000

    admin側店舗関連実装

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -4,17 +4,48 @@ class Admin::StoresController < ApplicationController
   end
 
   def create
+    @store = Store.new(store_params)
+      if @store.save
+        flash[:notice] = '店舗登録成功です'
+        redirect_to admin_store_path(@store.id)
+      else
+        flash[:alert] = '店舗登録エラー'
+        render :new
+      end
   end
 
   def index
+    @stores = Store.page(params[:page]).per(10)
   end
 
   def show
+    @store = Store.find(params[:id])
   end
 
   def edit
+    @store = Store.find(params[:id])
   end
 
   def update
+    @store = Store.find(params[:id])
+      if @store.update(store_params)
+        flash[:notice] = "店舗登録編集完了です"
+        redirect_to admin_store_path(@store)
+      else
+        flash[:alert] = "店舗登録編集エラー"
+        render :edit
+      end
+  end
+
+  private
+  def store_params
+    params.require(:store).permit(:name,
+                                  :address,
+                                  :tell_number,
+                                  :price,
+                                  :business_hours,
+                                  :content,
+                                  :image
+                                  )
   end
 end
