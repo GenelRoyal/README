9eb7b134251f9000ec6fd0019539ee68a2dd8a62
EC2 Default User <ec2-user@ip-172-31-47-229.ap-northeast-1.compute.internal>
2024-11-10 05:48:39 +0000

    store.migrate

--- db/migrate/20241102121047_create_stores.rb
+++ db/migrate/20241102121047_create_stores.rb
@@ -4,7 +4,7 @@ class CreateStores < ActiveRecord::Migration[6.1]
       t.string :name, null: false, default: ""
       t.text :address, null: false
       t.string :tell_number, null: false, default: ""
-      t.integer :price, null: false, default: ""
+      t.integer :price, null: false
       t.text :business_hours, null: false
       t.text :content, null: false
       t.timestamps
