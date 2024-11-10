0c8b2ecf2e6fd9d4571243ea4166361620094889
EC2 Default User <ec2-user@ip-172-31-47-229.ap-northeast-1.compute.internal>
2024-11-10 08:03:34 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
- Admin.create!(email: "admin@example.jp",
-               password:  "123456"
-               )
+Admin.find_or_create_by!(email: "admin@example.jp") do |admin|
+  admin.password = "123456"
+  admin.password_confirmation = "123456"
+end
