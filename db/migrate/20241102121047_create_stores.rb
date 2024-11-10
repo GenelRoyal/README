6448d2e7c6c6bdac0b524d533c3a4ea987b7fe78
EC2 Default User <ec2-user@ip-172-31-47-229.ap-northeast-1.compute.internal>
2024-11-10 05:44:29 +0000

    store.migrate

--- db/migrate/20241102121047_create_stores.rb
+++ db/migrate/20241102121047_create_stores.rb
@@ -2,11 +2,11 @@ class CreateStores < ActiveRecord::Migration[6.1]
   def change
     create_table :stores do |t|
       t.string :name, null: false, default: ""
-      t.text :address, null: false, default: ""
+      t.text :address, null: false
       t.string :tell_number, null: false, default: ""
       t.integer :price, null: false, default: ""
-      t.text :business_hours, null: false, default: ""
-      t.text :content, null: false, default: ""
+      t.text :business_hours, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
