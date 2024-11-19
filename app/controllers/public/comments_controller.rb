10c8cc68668898c4e82aad07fca4add460c2d387
EC2 Default User <ec2-user@ip-172-31-46-66.eu-north-1.compute.internal>
2024-11-19 07:53:25 +0000

    カラム日本語化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,13 +2,18 @@ class Public::CommentsController < ApplicationController
   before_action :set_review
 
   def create
+    @review = Review.find(params[:review_id]) # 再描画用
+    @store = @review.store                   # 再描画用
+    @review_tags = @review.tags              # 再描画用
+    @comments = @review.comments.includes(:user) # 再描画用
+
     @comment = @review.comments.build(comment_params)
     @comment.user = current_user  # ログイン中のユーザーをコメントの投稿者として設定
 
     if @comment.save
       redirect_to store_review_path(@review.store, @review), notice: 'コメントが投稿されました。'
     else
-      flash.now[:alert] = 'コメントの投稿に失敗しました。'+ @comment.errors.full_messages.join(", ")
+      flash[:alert] = 'コメントの投稿に失敗しました。'+ @comment.errors.full_messages.join(", ")
       render 'public/reviews/show'
     end
   end
